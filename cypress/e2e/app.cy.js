8fdb55f451666fb30caad3dae49dbec2e325d813
Nehuuln <maximeye1234@gmail.com>
2026-01-15 16:25:40 +0100

    simplify code

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,25 +1,13 @@
 describe('Tests E2E - Application Flask', () => {
   
-  it('should check the availability of the application', () => {
+    it('checking health endpoint', () => {
     cy.request('/health').then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.eq('OK');
     });
   });
 
-  it('should return 404 for a non-existent route', () => {
-    cy.request({
-      url: '/non-existent-route',
-      failOnStatusCode: false
-    }).then((response) => {
-      expect(response.status).to.eq(404);
-    });
-  });
-
-});
-
-describe('Counter functionality', () => {
-  it('should increment counter when button is clicked', () => {
+  it('testing counter', () => {
     cy.visit('/');
     
     cy.get('#counter').should('have.text', '0');
@@ -32,8 +20,21 @@ describe('Counter functionality', () => {
     cy.get('#counter').should('have.text', '3');
   });
 
-  it('should display the page title', () => {
+  it('checking page display', () => {
     cy.visit('/');
+    
     cy.contains('Compteur').should('be.visible');
+    
+    cy.contains('+1').should('be.visible');
   });
+
+  it('should return 404 for a non-existent route', () => {
+    cy.request({
+      url: '/route-inexistante',
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
 });
